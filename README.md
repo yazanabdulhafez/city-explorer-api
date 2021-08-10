9f0e9bfea7fea0f0fcded1883713c90a3f1225c4
yazanabdulhafez <yazanali31@yahoo.com>
2021-08-10 21:42:01 +0300

    finsh the API

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # city-explorer-api
 
-# Project Name
+## Project Name
 
-**Author**: Your Name Goes Here
+**Author**: Yazan Alkharabsheh
 **Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
 
 ## Overview
@@ -15,20 +15,37 @@
 <!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
 
 ## Change Log
+
 <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
 
 01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+08-08-2021 7:00pm - Application now has a fully-functional express server
 
 ## Credit and Collaborations
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+1-Mohmmad Al selwide 2-Balqees Al-fasatleh.
 
+-----------------------------------------
 
-Name of feature: ________________________________
+Name of feature: see weather info for the city
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: 60 min
 
-Start time: _____
+Start time: 8/8/2021 - 2:00
 
-Finish time: _____
+Finish time: 8/8/2021 - 3:30
 
-Actual time needed to complete: _____
+Actual time needed to complete: 1.5 hour
+
+-----------------------------------------
+Name of feature: handle the server error
+
+Estimate of time needed to complete: 50 min
+
+Start time: 8/8/2021 - 3:30
+
+Finish time: 8/8/2021 - 6:20
+
+Actual time needed to complete: 170 min
+
+-----------------------------------------
--- server.js
+++ server.js
@@ -6,55 +6,51 @@ const weatherData = require('./data/weather.json')
 app.use(cors());
 require('dotenv').config();
 const PORT = process.env.PORT;
-// const axios = require('axios');
 
 /////////////////////* create the end point*///////////////////////
 
-//http://localhost:8000/weather?searchQuery=Amman&lon= 35.91&lat=31.95
+//`http://localhost:8000/weather?searchQuery=${this.state.cityName}&lon=${this.state.lon}&lat=${this.state.lat}`
+//http://localhost:8000/weather?searchQuery=amman&lon=35.9239625&lat=31.9515694
+
 app.get('/weather', (req, res) => {
 
   let city = req.query.searchQuery;
   let lat = req.query.lat;
   let lon = req.query.lon;
-  try {
 
-    let findCity = weatherData.find((element) => {
-      console.log(element);
-      if (element.city_name.toLowerCase() === city.toLowerCase() &&
-        element.lat === Number(lat) && element.lon === Number(lon)) {
-        return element;
-      }
-    })
-
-    if (findCity !== undefined) {
-      let shownData = [];
-
-      findCity.data.map((city) => {
-        shownData.push(
-          new Forecast(
-            city.weather.description,
-            city.datetime,
-            city.app_min_temp,
-            city.app_max_temp
-          )
-        );
-      });
-      res.status(200).send(shownData);
-    } else if (findCity === undefined) {
-      res.send(`cant found ${city} use (Amman,Paris,Seattle)`);
+  let findCity = weatherData.find((element) => {
+    console.log(element);
+    if (element.city_name.toLowerCase() === city.toLowerCase() &&
+      element.lat === Number(lat) && element.lon === Number(lon)) {
+      return element;
     }
-  } catch (e) {
-    let err = {
-      error: `cant found ${city} use (Amman,Paris,Seattle)`
-    }
-    res.status(404).send(err)
+  })
+
+  console.log(findCity);
+  if (findCity !== undefined) {
+    let shownData = [];
+
+    findCity.data.map((city) => {
+      shownData.push(
+        new Forecast(
+          city.weather.description,
+          city.datetime,
+          city.app_min_temp,
+          city.app_max_temp
+        )
+      );
+    });
+    res.status(200).send(shownData);
+
+  } else if (findCity === undefined) {
+    res.send(`cant found ${city} use (Amman,Paris,Seattle)`);
   }
 })
 
 /////////////////////* the class component *///////////////////////
 
 class Forecast {
-  constructor( description,date,minTemp,maxTemp) {
+  constructor(description, date, minTemp, maxTemp) {
     this.description = `Low of ${minTemp}, high of ${maxTemp} with ${description}`;
     this.date = date;
   }
@@ -64,5 +60,7 @@ class Forecast {
 
 app.listen(PORT, () => {
   console.log(`you are veiwing port : ${PORT}`);
-}); // kick start the express server to work
+});
+
+
 
